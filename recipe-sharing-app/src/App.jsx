16a0927798969721d81a7062567368d03e653773
Isiaka Maryam <isiakamaryam457@gmail.com>
2025-07-29 02:31:43 -0700

    wow

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,15 +1,31 @@
-import { useState } from 'react'
-import AddRecipeForm from './components/AddRecipeForm'
-import RecipeList from './components/RecipeList'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AddRecipeForm from './components/AddRecipeForm';
+import RecipeList from './components/RecipeList';
+import RecipeDetails from './components/RecipeDetails';
 
 function App() {
-  
   return (
-    <>
-      <RecipeList />
-      <AddRecipeForm />
-    </>
-  )
+    <Router>
+      <div style={{ padding: '20px', maxWidth: '800px', margin: '0 auto' }}>
+        <Routes>
+          {/* Home page route */}
+          <Route 
+            path="/" 
+            element={
+              <div>
+                <h1>Recipe Sharing Application</h1>
+                <AddRecipeForm />
+                <RecipeList />
+              </div>
+            } 
+          />
+          
+          {/* Recipe details route */}
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
