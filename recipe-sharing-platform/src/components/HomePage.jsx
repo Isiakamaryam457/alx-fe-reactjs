4d6e6cc35f9727a7c484a9575b4017fa4be13c12
Isiaka Maryam <isiakamaryam457@gmail.com>
2025-08-14 04:30:09 -0700

    adding responsiveness

--- recipe-sharing-platform/src/components/HomePage.jsx
+++ recipe-sharing-platform/src/components/HomePage.jsx
@@ -40,7 +40,7 @@ const HomePage = () => {
     return (
         <div className="">
             <h1 className="flex mb-10 text-red-400">Recipe sharing platform</h1>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-10">
                 {recipes.map((recipe) => (
                     <div key={recipe.id} className="bg-red-400
                      text-black p-10 rounded-lg
